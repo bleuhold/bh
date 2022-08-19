2945bc562c37b9d56b830c8aa4ef4b5458dff1d5
Johannes Scribante <johannes.scribs@gmail.com>
2022-08-19 20:11:05 +0200

    feat/15 - added the info struct

--- cmds/info.go
+++ cmds/info.go
@@ -1,9 +1,14 @@
 package cmds
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/bleuhold/bh/cmd"
+	"github.com/bleuhold/bhman/fs"
+	"github.com/google/uuid"
+	"log"
+	"time"
 )
 
 var INFO *cmd.Command
@@ -33,4 +38,26 @@ func execute(cmd *cmd.Command) {
 	default:
 		cmd.PrintHelp()
 	}
+}
+
+type Info struct {
+	PropertyUUID uuid.UUID `json:"propertyUUID"`
+	StartDate    time.Time `json:"startDate"`
+	EndDate      time.Time `json:"endDate"`
+}
+
+// Print prints the info data to the CLI.
+func (i *Info) Print() {
+	fmt.Printf("%-15s %v\n", "Property UUID:", i.PropertyUUID)
+	fmt.Printf("%-15s %v\n", "Start Date:", i.StartDate)
+	fmt.Printf("%-15s %v\n", "End Date:", i.EndDate)
+}
+
+// Save writes the data to the file system.
+func (i *Info) Save() {
+	xb, err := json.Marshal(i)
+	if err != nil {
+		log.Fatalf("Unable to marshal info data: %v", err)
+	}
+	fs.WriteFile("info.json", xb)
 }
